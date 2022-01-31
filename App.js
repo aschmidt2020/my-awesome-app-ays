8757bfc8a92e936c146b8af4f64fdcbd51272e6c
Audrey Schmidt <audsch@outlook.com>
2022-01-31 14:33:10 -0600

    fix: fixed multi-line commands in base-image-options.yml

--- App.js
+++ App.js
@@ -8,6 +8,6 @@ const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
     console.log(`Running app at port:${APP_PORT}.)
 })
 
-// This is a change to branch feat1 to demonstrate release functionality using GitHub
-// We will need to add some notes to our PR in order to demo the automated notes function as well.
 // Simple Node 'Hello World' app for testing Github CI/CD features - added github workflows
+// Added additional workflow with echo commands
+// We will need to add some notes to our PR in order to demo the automated notes function as well.
